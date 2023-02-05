530a18b9536831853530246f3be4bf7df1b90014
nem0z <maxime.bernard.dev@gmail.com>
2023-02-05 02:48:24 +0100

    feat (message): implement class Message

---
+++ message.py
@@ -0,0 +1,20 @@
+import hashlib
+
+import utils
+
+class Message:
+    def __init__(self, command_name, payload):
+        self.magic_number = bytes.fromhex("f9beb4d9")
+        self.command_name = utils.to_bytes_fixed_size(command_name.encode("ascii"), 12)
+        self.payload = payload
+        self.length = (int(len(payload))).to_bytes(4, byteorder="little", signed=False)
+        self.checksum = hashlib.sha256(hashlib.sha256(payload).digest()).digest()[:4]
+
+    def get(self):
+        return (
+            self.magic_number
+            + self.command_name
+            + self.length
+            + self.checksum
+            + self.payload
+        )
