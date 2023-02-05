e28edf0a469d39b4e9098edb4d31f8ee930e4a34
nem0z <maxime.bernard.dev@gmail.com>
2023-02-05 02:46:44 +0100

    feat (client): implement class Client

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket
+
+from messages import ping
+from messages import version
+
+class Client():
+    def __init__(self, peer_ip, peer_port):
+        self.peer_ip = peer_ip
+        self.peer_port = peer_port
+        self.sock = socket.socket()
+
+    def connect(self):
+        self.sock.connect((self.peer_ip, self.peer_port))
+        version_message = version.message(self.peer_ip)
+        self.sock.send(version_message.get())
+        return self.sock.recv(24)
+
+    def ping(self):
+        ping_message = ping.message()
+        self.sock.send(ping_message.get())
+        return self.sock.recv(128)
