f20cb3869451b48b61d2dce80654f5e285fcf4a9
nem0z <maxime.bernard.dev@gmail.com>
2023-02-13 11:27:02 +0100

    feat (main): update default node address

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 from client import Client
 
 if __name__ == "__main__":
-    client = Client("178.83.160.129", 8333)
+    client = Client("23.88.68.160", 8333)
     version = client.connect()
 
     pong = client.ping()
